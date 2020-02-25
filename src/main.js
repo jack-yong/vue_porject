eb3352eedbf4d7cadda27c4ea97c3295455183dc
jack-yong <1807413446@qq.com>
2020-02-26 01:03:27 +0800

    梳理项目架构

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import {
-  Actionsheet
-} from 'vux'
 
-Vue.component('actionsheet', Actionsheet)
 Vue.config.productionTip = false
 
 new Vue({
